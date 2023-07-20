e04b21f853c16d12c15f594c6e705c9f23aba9e2
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2023-07-20 10:07:17 +0900

    feat: 言語と実行環境の判別

--- src/execute.ts
+++ src/execute.ts
@@ -2,9 +2,12 @@
 
 import { entities as misskeyEntities } from "misskey-js";
 import { GAS } from "./common";
-import { RunResult, Runner } from "./runner";
+import { Langs, RunResult, Runner, Runners } from "./runner";
 
-declare const runners: { [key: string]: Runner };
+declare const langs: Langs;
+declare function determineLang(s: string): keyof Langs | null;
+declare const runners: Runners;
+declare function determineRunners(lang: keyof Langs): (keyof Runners)[];
 
 const commands: {
   [key: string]: {
@@ -76,21 +79,54 @@ function run(params: string): string {
     }
     const ext: string = codeBlock[0].match(/(?<=^```).*/)![0].toLowerCase();
     const code: string = codeBlock[0].match(/(?<=\n).+(?=\n)/s)![0];
-    const args: string = params
+    const args: string[] = params
       .slice(0, codeBlock.index!)
       .normalize("NFKC")
       .toLowerCase()
-      .trim();
-    // TODO: 言語と実行環境とコンパイラを調べる
-    if (args !== "") throw new Error("現状 Text しかできないんだ！ごめんね！");
-    const result: RunResult = runners["local"]!("text", code);
+      .trim()
+      .split(/\s+/);
+    let runnerName: keyof Runners | null = null;
+    let lang: keyof Langs | null = null;
+    for (const arg of args) {
+      if (arg === "") continue;
+      if (runnerName == null && runners.hasOwnProperty(arg)) {
+        runnerName = arg as keyof Runners;
+        continue;
+      }
+      if (lang == null) {
+        lang = determineLang(arg);
+        if (lang != null) continue;
+      }
+      throw new Error(`「${arg}」ってオプションは無効だよ。`);
+    }
+    if (lang == null) {
+      lang = determineLang(ext);
+    }
+    if (lang == null) {
+      throw new Error("言語を指定してね。");
+    }
+    if (runnerName == null) {
+      runnerName = determineRunners(lang)[0];
+      if (runnerName == null) {
+        throw new Error(`${lang} 言語に対応した実行環境は無いっぽいよ！`);
+      }
+    }
+    const runner = runners[runnerName];
+    if (!runner.langs.some((l) => lang === l)) {
+      throw new Error(`${runnerName} は ${lang} 言語に対応してないよ！`);
+    }
+    // run code
+    const result: RunResult = runner.run(
+      lang as typeof runner extends Runner<infer P> ? P : never,
+      code
+    );
     return (
       "標準出力:" +
       (result.stdout ? "\n```\n" + result.stdout + "\n```\n" : "\n") +
       "標準エラー出力:" +
       (result.stderr ? "\n```\n" + result.stderr + "\n```\n" : "\n") +
       (result.exitCode ? `終了コード: ${result.exitCode}\n` : "") +
-      `言語: ${"Text"} (${"Local"})\n` +
+      `言語: ${lang} (${runnerName})\n` +
       `コード長: ${Utilities.newBlob(code).getBytes().length} Byte\n` +
       `結果: ${result.status}\n` +
       (result.execTime != null ? `実行時間: ${result.execTime} ms\n` : "") +
--- src/runner.ts
+++ src/runner.ts
@@ -1,18 +1,66 @@
 // SPDX-License-Identifier: MIT
 
-export interface RunResult {
+const langs = {
+  brainfuck: {
+    names: ["brainfuck", "bf", "brainf*ck", "brainfu*k", "brainf**k"],
+  },
+  c: { names: ["c"] },
+  cpp: { names: ["c++", "cpp", "cc", "cxx"] },
+  python2: { names: ["python2", "py2"] },
+  python3: { names: ["python", "python3", "py", "py3"] },
+  text: { names: ["text", "txt", "cat"] },
+} satisfies { [key: string]: { names: string[] } };
+
+export type Langs = typeof langs;
+
+function determineLang(s: string): keyof Langs | null {
+  for (const l in langs) {
+    if (langs[l as keyof Langs].names.includes(s)) {
+      return l as keyof Langs;
+    }
+  }
+  return null;
+}
+
+export type RunResult = {
   status: "OK" | "CE" | "RE" | "TLE" | "MLE" | "IE";
   stdout: string;
   stderr: string;
   exitCode?: string;
   execTime?: number;
   memory?: number;
+};
+
+export class Runner<L extends keyof Langs> {
+  langs: L[];
+  run: (lang: L, code: string, input?: string) => RunResult;
+  constructor(langs: Runner<L>["langs"], run: Runner<L>["run"]) {
+    this.langs = langs;
+    this.run = run;
+  }
 }
 
-export type Runner = (lang: string, code: string, input?: string) => RunResult;
+const runners = {
+  local: new Runner(["text", "brainfuck"], (lang, code, input = "") => {
+    switch (lang) {
+      case "text":
+        return { status: "OK", stdout: code, stderr: "", execTime: 0 };
+      case "brainfuck":
+        throw new Error("brainfuck は実装予定だから、ちょっと待ってね。");
+    }
+  }),
+  wandbox: new Runner(["c", "cpp"], (lang, code, input = "") => {
+    throw new Error("Wandbox は対応予定だから、ちょっと待ってね。");
+  }),
+  paizaio: new Runner(["c", "cpp"], (lang, code, input = "") => {
+    throw new Error("PaizaIO は対応予定だから、ちょっと待ってね。");
+  }),
+} as const;
 
-const runners: { [key: string]: Runner } = {
-  local: (lang: string, code: string, input: string = ""): RunResult => {
-    return { status: "OK", stdout: code, stderr: "", execTime: 0 };
-  },
-};
+export type Runners = typeof runners;
+
+function determineRunners(lang: keyof Langs): (keyof Runners)[] {
+  return (Object.entries(runners) as [keyof Runners, Runners[keyof Runners]][])
+    .filter(([, runner]) => (runner.langs as (keyof Langs)[]).includes(lang))
+    .map(([name]) => name);
+}
